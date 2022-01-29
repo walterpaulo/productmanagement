0fe0372c2e14a39a4bed341a65c08e013c1d388a
walterpaulo <walter0paulo@hotmail.com>
2022-01-29 18:30:23 -0300

    new table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_183839) do
+ActiveRecord::Schema.define(version: 2022_01_29_212736) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
     t.float "purchase_price"
     t.float "sale_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
