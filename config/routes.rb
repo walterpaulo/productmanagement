b5c03c03cbc95a7f6dee9a93e786d4d5a7e394c2
walterpaulo <walter0paulo@hotmail.com>
2022-01-29 22:48:19 -0300

    add routes login

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+  get '/login', to: 'login#index'
+  get '/login/on', to: 'login#login'
+  get '/logout', to: 'login#logout'
 end
