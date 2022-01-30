1599ccd90f4ef7d7cf1d00a1f46110c25906a057
walterpaulo <walter0paulo@hotmail.com>
2022-01-29 23:49:06 -0300

    changed method to erify_authenticity_token.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :verify_authenticity_token 
+
+    def verify_authenticity_token
+        unless cookies[:product_admin].present?
+            redirect_to "/login"
+        end
+    end
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,13 +1,29 @@
 class LoginController < ApplicationController
+    skip_before_action :verify_authenticity_token 
 
 def index
     render layout: false
 end
 
 def login
+    if params[:email].present? && params[:password].present?
+        adm = User.where(email: params[:email], password: params[:password])
+        if adm.count > 0
+            cookies[:product_admin] = { value: adm.first.id, expires: 1.year.from_now, httponly: true }
+            redirect_to "/"
+        else
+            flash[:error] = "Invalid login and password!"
+            redirect_to "/login"
+        end
+    else
+        flash[:error] = "Invalid login and password!"
+        redirect_to "/login"
+    end
 end
 
 def logout
+    cookies[:product_admin] = nil
+    redirect_to "/login"
 end
 
 end
