360aca906d44188fa8749a35a3026001c82207bd
walterpaulo <walter0paulo@hotmail.com>
2022-01-29 23:47:40 -0300

    changed route verb /login/on to post

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   get '/login', to: 'login#index'
-  get '/login/on', to: 'login#login'
+  post '/login/on', to: 'login#login'
   get '/logout', to: 'login#logout'
 end
