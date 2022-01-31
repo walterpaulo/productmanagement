d684c73c780bff2afb42c05f6a4b8ae3a068921b
walterpaulo <walter0paulo@hotmail.com>
2022-01-31 12:58:30 -0300

    added bank return statement.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,14 +1,15 @@
-require "test_helper"
+require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  test "validates product creation" do
-    product = Product.new(name: "Rice", quantity: 2, purchase_price: 5, sale_price: 10)
+  test 'validates product creation' do
+    product = Product.new(name: 'Rice', quantity: 2, purchase_price: 5, sale_price: 22)
     assert product.save
+    product_count = Product.where(name: 'Rice')
+    assert(1, product_count)
   end
 
-  test "Create and validate fields" do
+  test 'Create and validate fields' do
     product = Product.new
     assert_not product.save
   end
-
 end
