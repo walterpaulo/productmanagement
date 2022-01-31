cf85306847e48ad3fdb93deaed94379f8d700ea3
walterpaulo <walter0paulo@hotmail.com>
2022-01-31 11:14:24 -0300

    created file to validate product fields.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validates product creation" do
+    product = Product.new(name: "Rice", quantity: 2, purchase_price: 5, sale_price: 10)
+    assert product.save
+  end
+
+  test "Create and validate fields" do
+    product = Product.new
+    assert_not product.save
+  end
+
 end
