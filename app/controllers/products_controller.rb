e0734aa0a3fd8f9c91c2319f855f4259abc690c8
walterpaulo <walter0paulo@hotmail.com>
2022-02-01 02:03:43 -0300

    add field image.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :quantity, :purchase_price, :sale_price)
+      params.require(:product).permit(:name, :quantity, :purchase_price, :sale_price, :image)
     end
 end
