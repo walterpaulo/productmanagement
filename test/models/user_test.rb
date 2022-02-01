693590cc45a48d7320a4f263ceb781fcd494533c
walterpaulo <walter0paulo@hotmail.com>
2022-02-01 18:28:10 -0300

    added tests, create, update, delete.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validates user creation' do
+    user = User.new(name: 'user', email: 'user@example.com.br', password: '123123')
+    assert user.save
+    user_count = User.where(name: 'user')
+    assert(1, user_count)
+  end
+
+  test 'Create and validate fields' do
+    product = User.new
+    assert_not product.save
+  end
+
+  test 'update user' do
+    user_update = User.find(1)
+    user_update.name = 'pedro'
+    user_update.save
+    assert User.where(name: 'pedro').present?
+  end
+
+  test 'delete product' do
+    user_delete = User.find(1)
+    user_delete.destroy
+    assert_not User.find_by(id: 1).present?
+  end
 end
