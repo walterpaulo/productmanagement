25ec5064331f18e68fe6dabf11d3c409386cda06
walterpaulo <walter0paulo@hotmail.com>
2022-02-03 11:21:51 -0300

    test validation correction.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,8 +4,8 @@ class UserTest < ActiveSupport::TestCase
   test 'validates user creation' do
     user = User.new(name: 'user', email: 'user@example.com.br', password: '123123')
     assert user.save
-    user_count = User.where(name: 'user')
-    assert(1, user_count)?
+    user_count = User.where(name: 'user').count
+    assert_equal(1, user_count)
   end
 
   test 'Create and validate fields' do
