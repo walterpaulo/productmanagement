a226aecf7f69551b4e652b54bf7341225aa44e1b
walterpaulo <walter0paulo@hotmail.com>
2022-02-03 22:51:41 -0300

    coluna alterada senha para password_digest.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_034909) do
+ActiveRecord::Schema.define(version: 2022_02_04_003025) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2022_02_01_034909) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
