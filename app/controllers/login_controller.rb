ebdf2b895ccd900583fccb627884c23008ad5ad1
walterpaulo <walter0paulo@hotmail.com>
2022-02-03 22:54:53 -0300

    changed login method.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,35 +1,34 @@
 class LoginController < ApplicationController
-    skip_before_action :verify_authenticity_token, :verify_login
-    layout "login"
+  skip_before_action :verify_authenticity_token, :verify_login
+  layout "login"
 
-def index
-end
+  def index; end
 
-def login
+  def login
     if params[:email].present? && params[:password].present?
-        adm = User.where(email: params[:email], password: params[:password])
-        if adm.count > 0
-            admin = adm.first
-            time = params[:remember] == "1"? 1.year.from_now : 30.minutes.from_now
-            value = {
-                id: admin.id,
-                name: admin.name,
-                email: admin.email
-            }
-            cookies[:product_admin] = { value: value.to_json, expires: time, httponly: true }
-            redirect_to "/products"
-        else
-            flash[:error] = "Invalid login and password!"
-            redirect_to "/login"
-        end
-    else
+      adm = User.where(email: params[:email]).last.authenticate(params[:password])
+      if adm.present?
+        admin = adm
+        time = params[:remember] == "1" ? 1.year.from_now : 30.minutes.from_now
+        value = {
+          id: admin.id,
+          name: admin.name,
+          email: admin.email
+        }
+        cookies[:product_admin] = { value: value.to_json, expires: time, httponly: true }
+        redirect_to "/products"
+      else
         flash[:error] = "Invalid login and password!"
         redirect_to "/login"
+      end
+    else
+      flash[:error] = "Invalid login and password!"
+      redirect_to "/login"
     end
-end
+  end
 
-def logout
+  def logout
     cookies[:product_admin] = nil
     redirect_to "/login"
-end
+  end
 end
