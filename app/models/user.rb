fe633765e609a31e08423e2ca4f58f501fa1f2c5
walterpaulo <walter0paulo@hotmail.com>
2022-02-10 14:32:47 -0300

    check email format and uniqueness, password with values between 6 to 20 characters.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+  before_validation { self.email = email.downcase}
   validates :name, :email, :password, :password_confirmation, presence: true
+  validates :password, :password_confirmation, length: { in: 6..20 }
   validates :password, confirmation: true
+  validates :email, uniqueness: true
+  validates :email, format: { with:/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "%{value} is invalid "}
   has_secure_password
 end
