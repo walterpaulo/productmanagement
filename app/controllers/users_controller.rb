618b404ddb0bef626d33361b65d172593f58d4f0
walterpaulo <walter0paulo@hotmail.com>
2022-02-10 21:29:22 -0300

    return attributes id, name email.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.paginate(page: params[:page], per_page: 5).order('created_at DESC')
+    @users = User.select(:id, :name, :email).paginate(page: params[:page], per_page: 5).order('created_at DESC')
   end
 
   # GET /users/1 or /users/1.json
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.select(:id, :name, :email).find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
