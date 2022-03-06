5c4d5fee318ebe651b616bfff65589077ef73ef5
walterpaulo <walter0paulo@hotmail.com>
2022-03-05 23:26:31 -0300

    modified files.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
+    email = params.as_json['session']['email']
+    password = params.as_json['session']['password']
+    user = User.find_by(email: email)
+    if user.password == password
+      log_in user
+      redirect_to produts_path
+    else
+      redirect_to login_url
+    end
   end
 
   def destroy
+    # Remove the user id from the session
+    session.delete(:current_user_id)
+    # Clear the memoized current user
+    @_current_user = nil
+    redirect_to login_url
   end
 end
