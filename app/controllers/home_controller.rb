e1b7a262b760d025135cbd873cfa3ec5cf4c5df3
walterpaulo <walter0paulo@hotmail.com>
2022-03-06 22:02:05 -0300

    removed skip_before_action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
-    skip_before_action :verify_authenticity_token
-    layout "home"
-    def index
-        @products = Product.paginate(page: params[:page], per_page: 8).order('created_at DESC')
-    end
+  layout "home"
+  def index
+    @products = Product.paginate(page: params[:page], per_page: 8).order('created_at DESC')
+  end
 end
